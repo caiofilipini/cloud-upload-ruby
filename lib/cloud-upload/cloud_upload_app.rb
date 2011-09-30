961069c087856dc567b25b18c80676e0d248bc06
Caio Filipini <caiofilipini@gmail.com>
2011-09-30 11:38:56 -0300

    improved specs structure

--- lib/cloud-upload/cloud_upload_app.rb
+++ lib/cloud-upload/cloud_upload_app.rb
@@ -1,5 +1,5 @@
 class CloudUploadApp
   def call(env)
-    [200, {"Content-Type" => "text/plain"}, ""]
+    []
   end
 end
--- spec/lib/cloud-upload/cloud_upload_app_spec.rb
+++ spec/lib/cloud-upload/cloud_upload_app_spec.rb
@@ -2,9 +2,23 @@ require File.expand_path("spec/spec_helper")
 
 describe CloudUploadApp do
 
-  it "should be a Rack application" do
-    env = {}
-    subject.call(env).should == [200, {"Content-Type" => "text/plain"}, ""]
+  context "Rack application" do
+    it "should respond to call()" do
+      subject.should respond_to :call
+    end
+
+    it "should be able to receive environment data as a Hash and return an Array" do
+      subject.call(Hash.new).should be_a Array
+    end
+  end
+
+  context "Route handling" do
+
+    context "/" do
+      it "should call FormHandler" do
+      end
+    end
+
   end
 
 end
