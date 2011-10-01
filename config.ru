142bf9307af8a4f2f74a02b52e9b4cbae0142e44
Caio Filipini <caiofilipini@gmail.com>
2011-10-01 13:48:51 -0300

    added static files support and moved form HTML to its own file

--- config.ru
+++ config.ru
@@ -1,7 +1,12 @@
 $:.push File.expand_path("../lib", __FILE__)
+$:.push File.expand_path(".", __FILE__)
 
 require "rack"
 require "cloud_upload"
+
+use Rack::Static,
+  :root => "public",
+  :urls => ["/js"]
 
 run CloudUploadApp.new
 
--- lib/cloud_upload/handler/form_handler.rb
+++ lib/cloud_upload/handler/form_handler.rb
@@ -7,27 +7,6 @@ class FormHandler
   private
 
   def form_html
-    html = <<-HTML
-<html>
-  <head>
-    <title>Cloud Upload</title>
-  </head>
-  <body>
-    <h1>Upload your music!</h1>
-    <div id="main">
-      <form id="uploadForm" action="#" method="post" enctype="multipart/form-data">
-        <input id="uid" type="hidden" name="userId" value="" />
-        <input type="file" name="fileToUpload" id="fileToUpload" />
-      </form>
-    </div>
-
-    <form id="detailsForm" action="#" method="post">
-      <textarea name="details"></textarea>
-      <input id="submit" type="submit" value="Save" />
-    </form>
-  </body>
-</html>
-    HTML
-    html
+    File.open("public/form.html", File::RDONLY).read
   end
 end
