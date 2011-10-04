8dd72eca4f3b9056d2d7b4ffe352509541a365bf
Caio Filipini <caiofilipini@gmail.com>
2011-10-04 10:45:47 -0300

    refactored form_handler to separate things a little bit

--- lib/cloud_upload/handler/form_handler.rb
+++ lib/cloud_upload/handler/form_handler.rb
@@ -1,12 +1,22 @@
 class FormHandler
+  FORM_HTML_FILE = "public/form.html"
+
   def handle(request)
     return [405, {}, ""] unless request.get?
-    [200, {"Content-Type" => "text/html"}, StringIO.new(form_html)]
+    [200, {"Content-Type" => "text/html"}, StringIO.new(html_with_upload_id)]
   end
 
   private
 
-  def form_html
-    File.open("public/form.html", File::RDONLY).read
+  def html_with_upload_id
+    read_html_file.gsub(/{{uid}}/, new_upload_id)
+  end
+
+  def read_html_file
+    File.open(FORM_HTML_FILE, File::RDONLY).read.to_s
+  end
+
+  def new_upload_id
+    Time.now.to_i.to_s
   end
 end
